74dc4cff6d0312bd2fcdf7ea0b6aab7f7add10e5
daveHylde <david@jobbi.no>
2024-03-11 08:22:42 +0100

    razor

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,11 +1,11 @@
 return {
   "mfussenegger/nvim-dap",
   opts = function()
-    require('dap.ext.vscode').load_launchjs(nil, { netcoredbg = {'cs'} })
+    require("dap.ext.vscode").load_launchjs(nil, { netcoredbg = { "cs", "sln" } })
     require("dap").adapters["netcoredbg"] = {
       type = "executable",
       command = "/etc/profiles/per-user/david/bin/netcoredbg",
       args = { "--interpreter=vscode" },
     }
-  end
+  end,
 }
