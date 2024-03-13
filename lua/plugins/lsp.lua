38d23a89c5b4959b765c27f9c08a31c8e30e9c01
daveHylde <david@jobbi.no>
2024-03-13 22:38:01 +0100

    lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,8 @@
 local util = require("lspconfig.util")
 
 return {
+  { "Hoffs/omnisharp-extended-lsp.nvim" },
+  {
   "neovim/nvim-lspconfig",
   dependencies = {
     "jose-elias-alvarez/typescript.nvim",
@@ -27,6 +29,26 @@ return {
         root_dir = util.root_pattern("angular.json", "project.json"), -- This is for monorepo's
         filetypes = { "html", "typescript", "typescriptreact", "angular" },
       },
+      omnisharp = {
+        handlers = {
+          ["textDocument/definition"] = function(...)
+            return require("omnisharp_extended").handler(...)
+          end,
+        },
+        keys = {
+          {
+            "gd",
+            function()
+              require("omnisharp_extended").telescope_lsp_definitions()
+            end,
+            desc = "Goto Definition",
+          },
+        },
+        enable_roslyn_analyzers = true,
+        organize_imports_on_format = true,
+        enable_import_completion = true,
+      },
     },
   },
+  }
 }
