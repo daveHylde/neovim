ed03d4c32885a7f0fb0edd663f22611fc08d90d8
daveHylde <david@jobbi.no>
2024-03-29 14:06:38 +0100

    conform

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
+
 local opt = vim.opt
 
 opt.relativenumber = false
@@ -10,3 +10,4 @@ opt.wrap = true
 opt.swapfile = false
 
 vim.g.minipairs_disable = true
+
