25da48005d9fbfe1a470c7ee1367ec7c9d2cade1
daveHylde <david@jobbi.no>
2024-04-16 08:55:24 +0200

    harpoon overwrite

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,75 +1,48 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  opts = {
-    menu = {
-      width = vim.api.nvim_win_get_width(0) - 4,
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    opts = {
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+      },
     },
-  },
-  keys = {
-    {
-      "<leader>ha",
-      function()
-        require("harpoon"):list():add()
-      end,
-      desc = "Harpoon file",
-    },
-    {
-      "<leader>hd",
-      function()
-        require("harpoon"):list():remove()
-      end,
-      desc = "remove Harpoon file",
-    },
-    {
-      "<leader>hc",
-      function()
-        require("harpoon"):list():clear()
-      end,
-      desc = "Clear Harpoon list",
-    },
-    {
-      "<leader>hh",
-      function()
-        local harpoon = require("harpoon")
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end,
-      desc = "Harpoon quick menu",
-    },
-    {
-      "<C-1>",
-      function()
-        require("harpoon"):list():select(1)
-      end,
-      desc = "Harpoon to file 1",
-    },
-    {
-      "<C-2>",
-      function()
-        require("harpoon"):list():select(2)
-      end,
-      desc = "Harpoon to file 2",
-    },
-    {
-      "<C-3>",
-      function()
-        require("harpoon"):list():select(3)
-      end,
-      desc = "Harpoon to file 3",
-    },
-    {
-      "<C-4>",
-      function()
-        require("harpoon"):list():select(4)
-      end,
-      desc = "Harpoon to file 4",
-    },
-    {
-      "<C-5>",
-      function()
-        require("harpoon"):list():select(5)
-      end,
-      desc = "Harpoon to file 5",
-    },
+    keys = function()
+      local keys = {
+        {
+          "<leader>ha",
+          function()
+            require("harpoon"):list():add()
+          end,
+          desc = "Harpoon File",
+        },
+        {
+          "<leader>hc",
+          function()
+            require("harpoon"):list():clear()
+          end,
+          desc = "Clear Harpoon List",
+        },
+        {
+          "<leader>hh",
+          function()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "Harpoon Quick Menu",
+        },
+      }
+
+      for i = 1, 5 do
+        table.insert(keys, {
+          "<c-" .. i .. ">",
+          function()
+            require("harpoon"):list():select(i)
+          end,
+          desc = "Harpoon to File " .. i,
+        })
+      end
+      return keys
+    end,
   },
 }
