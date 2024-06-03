e52f7fb058a5b64f87853bd5d5ff6b4910d48542
daveHylde <david@jobbi.no>
2024-06-03 21:27:45 +0200

    trouble

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,11 +1,11 @@
 return {
-  { "Issafalcon/neotest-dotnet" },
   {
     "nvim-neotest/neotest",
     dependencies = {
       "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      "Issafalcon/neotest-dotnet",
     },
     opts = {
       adapters = {
