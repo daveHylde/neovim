f8af10dd2e714b791e6712917d99b9ff7c27763c
daveHylde <david@jobbi.no>
2024-06-13 15:51:30 +0200

    netcoredbg bug

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -4,7 +4,7 @@ return {
     require("dap.ext.vscode").load_launchjs(nil, { netcoredbg = { "cs", "sln" } })
     require("dap").adapters["netcoredbg"] = {
       type = "executable",
-      command = "netcoredbg",
+      command = "/etc/profiles/per-user/david/bin/netcoredbg",
       args = { "--interpreter=vscode" },
     }
   end,
