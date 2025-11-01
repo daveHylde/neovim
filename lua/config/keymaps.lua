e4dd5643e1442cfe2d68c4816dcf2419a3a1d38e
David Hyldgaard <david@jobbi.no>
2025-11-01 01:58:37 +0100

    easy

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,15 @@ vim.keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window" })
 
 -- Navigate away from Claude Code
 vim.keymap.set("n", "<C-y>", "<C-w>p", { desc = "Go to Previous Window" })
+
+-- Move to window using the <alt> yneo keys
+vim.keymap.set("n", "<c-Left>", ":TmuxNavigateLeft<CR>", { desc = "Go to Left Window", silent = true, remap = true })
+vim.keymap.set("n", "<c-Down>", ":TmuxNavigateDown<CR>", { desc = "Go to Lower Window", silent = true, remap = true })
+vim.keymap.set("n", "<c-Up>", ":TmuxNavigateUp<CR>", { desc = "Go to Upper Window", silent = true, remap = true })
+vim.keymap.set("n", "<c-Right>", ":TmuxNavigateRight<CR>", { desc = "Go to Right Window", silent = true, remap = true })
+vim.keymap.set(
+  "n",
+  "<c-p>",
+  ":TmuxNavigatePrevious<CR>",
+  { desc = "Go to Previous Window", silent = true, remap = true }
+)
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "RRethy/vim-illuminate", enabled = false },
+  { "ckipp01/stylua-nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+}
