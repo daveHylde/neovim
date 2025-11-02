09589a306528c12f5196744d6cc538f4957a278a
David Hyldgaard <david@jobbi.no>
2025-11-02 22:53:29 +0100

    dotenv

---
+++ lua/plugins/dotenv.lua
@@ -0,0 +1,11 @@
+return {
+  "ellisonleao/dotenv.nvim",
+  lazy = false, -- Load on start
+  config = function()
+    require("dotenv").setup({
+      enable_on_load = true, -- will load your .env file upon loading a buffer
+      verbose = false, -- show error notification if .env file is not found and if .env is loaded
+      file_name = ".env.local", -- will override the default file name '.env'
+    })
+  end,
+}
