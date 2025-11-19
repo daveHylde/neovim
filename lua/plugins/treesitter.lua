975266dabc9713748ccae1bbf13fe831d6cee08f
David Hyldgaard <david.hyldgaard@gmail.com>
2025-11-19 12:37:58 +0100

    fix: treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,37 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  ---@class lazyvim.TSConfig: TSConfig
+  opts = {
+    -- LazyVim config for treesitter
+    indent = { enable = true }, ---@type lazyvim.TSFeat
+    highlight = { enable = true }, ---@type lazyvim.TSFeat
+    folds = { enable = true }, ---@type lazyvim.TSFeat
+    ensure_installed = {
+      "bash",
+      "c",
+      "c_sharp",
+      "diff",
+      "html",
+      "javascript",
+      "jsdoc",
+      "json",
+      "jsonc",
+      "lua",
+      "luadoc",
+      "luap",
+      "markdown",
+      "markdown_inline",
+      "printf",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    },
+  },
+}
