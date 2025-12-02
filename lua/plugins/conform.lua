7e472260902276cd54fd76245aed1384e64305d8
David Hyldgaard <david@jobbi.no>
2025-12-02 14:36:50 +0100

    csharpier

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,8 +1,32 @@
 return {
   "stevearc/conform.nvim",
   opts = {
+    async = true,
     formatters_by_ft = {
-      razor = { "lsp" },
+      cs = { "csharpier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      csproj = { "xmlformat" },
+      xml = { "xmlformat" },
+    },
+    formatters = {
+      xmlformat = {
+        command = "xmlformat",
+        -- args = { "--overwrite" },
+      },
+      csharpier = {
+        command = "csharpier",
+        args = {
+          "format",
+          "--write-stdout",
+        },
+        to_stdin = true,
+      },
+      -- format_on_save = {
+      --   -- These options will be passed to conform.format()
+      --   timeout_ms = 500,
+      --   lsp_fallback = true,
+      -- },
     },
   },
 }
