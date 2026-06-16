ec25747203febcdef57a871c902cdd4b53c32f1a
David Hyldgaard <david@jobbi.no>
2026-06-16 19:56:59 +0200

    fix

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 vim.opt.cursorline = false
 vim.opt.swapfile = false
-vim.g.minipairs_disable = true
+vim.g.minipairs_disable = false
 vim.g.autoformat = false
 
 -- -- Hide Roslyn's virtual/generated tree from completion/file prompts
--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -33,11 +33,4 @@ return {
       show_help = "<f1>",
     },
   },
-  -- -- 👇 if you use `open_for_directories=true`, this is recommended
-  -- init = function()
-  --   -- mark netrw as loaded so it's not loaded at all.
-  --   --
-  --   -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-  --   vim.g.loaded_netrwPlugin = 1
-  -- end,
 }
